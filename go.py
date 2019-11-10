df432203bb3d7bbb028eb8421ca80b6e518c25d4
R2D404 <ziwei.wei.r2d404@gmail.com>
2019-11-10 02:21:12 -0500

    almost done, test needed for the go game part

--- go.py
+++ go.py
@@ -1,69 +1,55 @@
 import numpy as np
-import math
 
 WHITE = 1
 EMPTY = 0
 BLACK = -1
 
 
-class group:
-    def __init__(self, board, color):
+class Group:
+    def __init__(self, board: "Board", color: int):
         self.board = board
-        self.group_positions = {}
-        self.liberty_positions = {}
         self.color = color
+        self.member_positions = set()
+        self.liberty_positions = set()
 
-    def is_member(self, x, y):
-        return (x, y) in self.group_positions
+    def print(self, x, y):
+        print_board = np.zeros([x, y], dtype=np.int8)
+        for position in self.member_positions:
+            print_board[position[0], position[1]] = 1
+        print(print_board)
 
-    def add_member(self, x, y):
-        self.group_positions[(x, y)] = True
+    def is_member(self, x: int, y: int):
+        return (x, y) in self.member_positions
+
+    def add_group(self, group: group):
+        self.member_positions.update(group.member_positions)
+        self.liberty_positions.update(group.liberty_positions)
+
+    # add_member assume the new member is free to add, and neighbor to the current group
+    def add_member(self, x: int, y: int):
+        self.member_positions.add((x, y))
+        for position in self.board.neighbors[(x, y)]:
+            if self.board.board[position] == EMPTY:
+                self.liberty_positions.add(position)
 
     def get_liberty(self):
-        return len(self.liberty_positions)*color
+        return len(self.liberty_positions)*self.color
 
-    def add_liberty_position(self, x, y):
-        self.liberty_positions[(x, y)] = True
-
-    def test_liberty_change(self, x, y):
-        # find same color neighbor
-        neighbors = self.board.neighbors[(x, y)]
-        free_positions = []
-        neighbor_groups = []
-        min_liberty = self.board.x * self.board.y
-        for neighbor in neighbors:
-            if self.board.positions[neighbor] == EMPTY:
-                free_positions.append(neighbor)
-            if self.board.positions[neighbor] == self.board.positions[(x, y)]:
-                neighbor_groups.append(
-                    self.board.map[neighbor].liberty_positions[neighbor])
-                min_liberty = math.min(
-                    len(self.board.map[neighbor].liberty_positions), min_liberty)
-
-        # calculate liberty after merge
-        new_liberty_positions = {}
-        liberty_change = len(new_liberty_positions) - min_liberty
-
-        for group in neighbor_groups:
-            new_liberty_positions.update(group.liberty_positions)
-        for position in free_positions:
-            if position in new_liberty_positions:
-                liberty_change -= 1
-        if (x, y) in new_liberty_positions:
-            liberty_change -= 1
-        return liberty_change
-
-    def contains(self, x, y):
-        return (x, y) in self.group_positions
+    def contains(self, x: int, y: int):
+        return (x, y) in self.member_positions
 
 
-class board:
-    def __init__(self, x, y):
+class Board:
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
-        self.map = {(i, j): None for i in range(x) for j in range(y)}
+        self.groups = set()
+        self.position_to_group = {}
         self.co = {BLACK: [], WHITE: []}
-        self.positions = {(i, j): EMPTY for i in range(x) for j in range(y)}
+
+        self.free_map = {(i, j): True for i in range(x) for j in range(y)}
+        self.board = np.zeros([x, y], dtype=np.int8)
+
         self.neighbors = {
             (x, y): list(
                 filter(
@@ -72,61 +58,97 @@ class board:
                 )
             ) for x, y in [(i, j) for i in range(x) for j in range(y)]
         }
-        self.diagonals = {
-            (x, y): list(
-                filter(
-                    self.__is_bound,
-                    [(x - 1, y + 1), (x + 1, y + 1),
-                     (x + 1, y - 1), (x - 1, y - 1)]
-                )
-            ) for x, y in [(i, j) for i in range(x) for j in range(y)]
-        }
-
-        self.board = np.zeros([x, y], dtype=np.int8)
 
     def print(self):
         print(self.board)
 
-    def which_group(self, x, y):
-        return self.map[(x, y)]
-
-    def __is_bound(self, x, y):
+    def __is_bound(self, x: int, y: int):
         return x % self.x == x and y % self.y == y
 
-    def get_neighbors_count(self, x, y):
+    def __is_EMPTY_free(self, x: int, y: int, color: int):
+        # can not place in co
+        if color == BLACK:
+            if (x, y) in self.co[BLACK]:
+                return False
+        elif color == WHITE:
+            if (x, y) in self.co[BLACK]:
+                return False
+        else:
+            raise ValueError("wrong color")
+
+        # count neighbors
+        neighbors_count = self.get_neighbors_count(x, y)
+
+        # can not suicide, can not reduce own liberty
+        if neighbors_count["diff"] == neighbors_count["total"]:
+            neighbor_groups = self.get_neighbor_groups(x, y)
+            for group in neighbor_groups:
+                if group.get_liberty() == 1:
+                    return True
+            return False
+        else:
+            return True
+
+    def is_free(self, x: int, y: int, color: int):
+        if self.board[(x, y)] != EMPTY:
+            return False
+        else:
+            return self.__is_EMPTY_free(x, y, color)
+
+    def update_free_map(self, color: int):
+        liberty_positions = set()
+        for group in self.groups:
+            liberty_positions.update(group.liberty_positions)
+
+        for position in liberty_positions:
+            color = self.board[position]
+            self.free_map[position] = self.__is_EMPTY_free(
+                position[0], position[1], color)
+
+    def add_stone(self, x: int, y: int, color: int):
+        self.board[x, y] = color
+        self.free_map[(x, y)] = False
+
+    # assume all groups are mergeable
+    def merge_groups(self, group_list: list, color: int):
+        if len(group_list) == 1:
+            return group_list[0]
+        merged = Group(self.board, color)
+        for curr_group in group_list:
+            for position in curr_group.member_positions:
+                self.position_to_group[position] = merged
+            merged.add_group(curr_group)
+            del curr_group
+        return merged
+
+    def get_liberty(self, x: int, y: int):
+        neighbors_count = self.get_neighbors_count(x, y)
+        return 4 - neighbors_count["total"]
+
+    def get_neighbors_count(self, x: int, y: int):
         same = 0
         diff = 0
         total = 0
         for neighbor in self.neighbors[(x, y)]:
-            if self.positions[neighbor] == self.positions[(x, y)]:
+            if self.board[neighbor] == self.board[(x, y)]:
                 same += 1
-            if self.positions[neighbor] == -self.positions[(x, y)]:
+            if self.board[neighbor] == -self.board[(x, y)]:
                 diff += 1
             total += 1
         return {"same": same, "diff": diff, "total": total}
 
-    def get_neighbors_by_group(self, x, y):
+    def get_neighbor_groups(self, x: int, y: int):
         neighbor_groups = []
         for neighbor in self.neighbors[(x, y)]:
-            neighbor_groups.append(neighbor)
+            if self.board[neighbor] != EMPTY:
+                group = self.position_to_group[neighbor]
+                neighbor_groups.append(group)
         return neighbor_groups
 
-    def get_diagonals_count(self, x, y):
-        same = 0
-        diff = 0
-        total = 0
-        for diagonal in self.diagonals[(x, y)]:
-            if self.diagonals[diagonal] == self.diagonals[(x, y)]:
-                same += 1
-            if self.diagonals[diagonal] == -self.diagonals[(x, y)]:
-                diff += 1
-            total += 1
-        return {"same": same, "diff": diff, "total": total}
-
-    def get_neighbors_liberty(self, x, y):
+    def get_neighbors_liberty(self, x: int, y: int):
         liberty = []
         for neighbor in self.neighbors[(x, y)]:
-            group = self.which_group(neighbor[0], neighbor[1])
+            group = self.position_to_group[neighbor]
             if group != None:
                 liberty.append(group.get_liberty())
             else:
@@ -134,76 +156,53 @@ class board:
         return liberty
 
 
-class game:
-    def __init__(self, x, y):
-        self.board = board(x, y)
-        self.position_to_group = {
-            (i, j): None for i in range(x) for j in range(y)
-        }
-        self.group_to_position = {}
+class Game:
+    def __init__(self, x: int, y: int):
+        self.board = Board(x, y)
         self.next_move = BLACK
 
-    def __get_neighbor_groups(self, x, y):
-        neighbor_groups = []
-        for neighbor in self.board.neighbors[(x, y)]:
-            if self.position_to_group[neighbor] != None:
-                neighbor_groups.append(self.position_to_group[neighbor])
-        return neighbor_groups
+    # new move will adjust the free_map
+    def __move(self, x: int, y: int, color: int):
+        if self.board.is_free(x, y, color):
 
-    def __is_free_for_black(self, x, y):
-        # can not place in co
-        if (x, y) in self.board.co[BLACK]:
+            # create a new group
+            new_group = Group(color, self.board)
+            new_group.add_member(x, y)
+
+            # find neighbor groups
+            neighbor_groups = self.board.get_neighbor_groups(x, y)
+            same_color_groups = set(new_group)
+
+            # update liberty for neighbor groups with different color, delete 0 liberty groups
+            exist_kill = False
+            for group in neighbor_groups:
+                if group.color == -color:
+                    group.liberty_positions.remove((x, y))
+                    if len(group.liberty_positions) == 0:
+                        exist_kill = True
+                        del group
+                elif group.color == color:
+                    same_color_groups.add(group)
+                else:
+                    raise ValueError("wrong color")
+
+            # merge same groups
+            self.board.merge_groups(same_color_groups, color)
+
+            # if there is kill then update all groups liberty board
+            if exist_kill == True:
+                pass
+
+            # update free map with
+            self.board.update_free_map(-color)
+        else:
             return False
 
-        # count neighbors
-        neighbors_count = self.board.get_neighbors_count(x, y)
-
-        # can not suicide
-        if neighbors_count["diff"] == neighbors_count["total"]:
-            neighbor_groups = self.__get_neighbor_groups(x, y)
-            for group in neighbor_groups:
-                if group != None and group.get_liberty() == 1:
-                    return True
-            return False
-
-        # can not reduce own liberty
-        if neighbors_count["same"] = 1:
-            neighbor_groups = self.__get_neighbor_groups(x, y)
-            for group in neighbor_groups:
-                if group != None and group.color == BLACK:
-
-                    return True
-
-        # when there is merge, can not reduce own liberty after merge
-
-        return True
-
-    def __is_free_for_white(self, x, y):
-        pass
-
-    def __black_move(self, x, y):
-        if self.board.positions[(x, y)] == EMPTY:
-
+    def move(self, x: int, y: int):
+        if self.__move(x, y, self.next_move) == True:
+            self.next_move *= -1
             return True
-        else:
-            return False
-        pass
-
-    def __white_move(self, x, y):
-        pass
-
-    def move(self, x, y):
-        if self.next_move == BLACK:
-            if self.__black_move(x, y) == True:
-                self.next_move *= -1
-                return True
-        elif self.next_move == WHITE:
-            if self.__white_move(x, y) == True:
-                self.next_move *= -1
-                return True
-        else:
-            raise ValueError("should be black or white move")
         return False
 
-    def start(self, print=False):
+    def start(self, print: bool = False):
         pass
